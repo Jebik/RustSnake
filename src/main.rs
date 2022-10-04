b2463d93142e48f9c2afc312273285eba40fd207
jebik88@gmail.com <jebik88@gmail.com>
2022-10-05 00:18:47 +0200

    Fix

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
+use std::ffi::CString;
+
 use miniquad::conf::Conf;
+use winapi::um::winuser::{MessageBoxA, MB_ICONINFORMATION, MB_OK};
 mod graphical_object;
 mod pos;
 mod images;
 mod game;
+
 
 //Create My miniquad
 //REMOVE ALL NON WINDOWS
 //REMOVE ALL UNUSED
 //SIMPLIFIE A LOT
 
-fn main() {
+fn main() {   
+    let mut message_body = "Vous avez trouver le snake cacher\n".to_owned();
+    message_body += "Utilisez les fleches pour tourner\n";
+    message_body += "Utilisez P pour mettre le jeu en pause\n";
+    message_body += "Utilisez Echap pour quitter\n";
+
+    let lp_text = CString::new(message_body).unwrap();
+    let lp_caption = CString::new("Felicitation").unwrap();
+    unsafe {
+        MessageBoxA(
+            std::ptr::null_mut(),
+            lp_text.as_ptr(),
+            lp_caption.as_ptr(),
+            MB_OK | MB_ICONINFORMATION
+        );
+    }
+
     miniquad::start(
         Conf {
             window_title: "AmbuSnake".to_string(),
