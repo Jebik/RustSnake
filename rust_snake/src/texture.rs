1a2772f9119dd77f90ee380792f82e3656e560ca
Benjamin LEDRAPPIER <bledrappier@synovo-group.com>
2022-10-21 15:39:14 +0200

    Testos

--- rust_snake/src/texture.rs
+++ rust_snake/src/texture.rs
@@ -5,12 +5,18 @@ pub struct TextureData
     pub height: u16,
     pub data: Vec<u8>,
 }
-
+extern crate bmp;
+use bmp::{Image, Pixel};
 pub fn get_texture(file_data: &[u8]) -> TextureData {
     let mut file = Decoder::new(file_data); 
     let pixels = file.decode().expect("failed to decode image");
     let metadata = file.info().unwrap();
-    
+    let mut img = Image::new(256, 256);
+
+    for (x, y) in img.coordinates() {
+        img.set_pixel(x, y, Pixel::new(255, 0, 0));
+    }
+    let _ = img.save("img.bmp");
     //LOADING IMAGE;
     let width:u16 = metadata.width as _;
     let height:u16 = metadata.height as _;
